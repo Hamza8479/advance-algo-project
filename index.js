1295029e0420ecb8262e675ed27f9c87ef588810
hamza <hamza.zaman@walee.pk>
2025-05-21 17:56:54 +0200

    removed unneccessary code

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 const {
-  addElement,
   display,
-  deleteElementByIndex,
-  updateElement,
-  sortByAttribute,
-  filterByAtribute,
   mainMenu
 } = require('./functions');
 
 display('Initial Dataset');
-
-// addElement('Mac M2', 1500, "grey", "2022-04-26");
-// display('After Adding Mac M2');
-
-// deleteElementByIndex(2);
-// display('After Deleting Index 2');
-
-// updateElement(1, 'Dell XPS 15', 1400, "black");
-// display('After Updating Index 1');
-
-// sortByAttribute('createdAt');
-// display('Sorted by createdAt');
-
-// sortByAttribute('price', 'desc');
-// display('Sorted by price');
-
-// const filtered = filterByAtribute(1000);
-// display('Filtered by price >= 1000', filtered);
 
 mainMenu()
